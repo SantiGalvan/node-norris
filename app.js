7c921d4cd5cc31f7047ac250a8d462a84965a3bc
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-27 19:52:12 +0200

    added fetch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const host = 'localhost';
-const port = '8080';
+const port = 8000;
 const fs = require("fs");
 const fetch = require("node-fetch");
 const path = require("path");
@@ -12,20 +12,44 @@ const readJSONData = (nomeFile) => {
     return JSON.parse(fileData);
 }
 
+// Scrittura file
+const writeJSONData = (nomeFile, newData) => {
+    const filePath = path.join(__dirname, nomeFile + '.json');
+    const fileString = JSON.stringify(newData);
+    fs.writeFileSync(filePath, fileString);
+}
+
+const isDuplicate = (jokesDb, value) => {
+    let found = false;
+    jokesDb.forEach(j => {
+        if (j.value === value) {
+            found = true;
+        }
+    });
+    return found;
+}
+
 const server = http.createServer((req, res) => {
+
     const norrisDb = readJSONData('norris-db');
-    res.writeHead(200, { "Content-Type": "txt/html; charset=utf-8" });
+
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
     // API
     fetch('https://api.chucknorris.io/jokes/random')
         .then(response => response.json())
         .then(data => {
-            let last = norrisDb > 0 ? norrisDb[norrisDb.length - 1] : 'Nessuna battuta disponibile';
-            let html = `<h3>${last}</h3>`;
+            if (!isDuplicate(norrisDb, data.value)) {
+                norrisDb.push({ value: data.value });
+                writeJSONData('norris-db', norrisDb);
+            }
+
+            let last = norrisDb.length > 0 ? norrisDb[norrisDb.length - 1].value : 'Nessuna battuta disponibile';
+            let norrisHtml = `<h3>${last}</h3>`;
+            res.end(norrisHtml);
         });
-    res.end()
 });
 
-server.listen((port, host) => {
+server.listen(port, host, () => {
     console.log(`Server avviato su http://${host}:${port}`);
 })
